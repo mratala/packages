95b4681ca134de8e3e2d67681e8e544e3d12c126
Mallikarjun <mratala@gmail.com>
2014-11-26 18:37:25 +0530

    Release1 packages

---
+++ TestPackageForCI/code/source/TestPackageForCI/utils.java
@@ -0,0 +1,40 @@
+package TestPackageForCI;
+
+// -----( IS Java Code Template v1.2
+// -----( CREATED: 2014-11-26 18:25:46 IST
+// -----( ON-HOST: MCMRAT01.eur.ad.sag
+
+import com.wm.data.*;
+import com.wm.util.Values;
+import com.wm.app.b2b.server.Service;
+import com.wm.app.b2b.server.ServiceException;
+// --- <<IS-START-IMPORTS>> ---
+// --- <<IS-END-IMPORTS>> ---
+
+public final class utils
+
+{
+	// ---( internal utility methods )---
+
+	final static utils _instance = new utils();
+
+	static utils _newInstance() { return new utils(); }
+
+	static utils _cast(Object o) { return (utils)o; }
+
+	// ---( server methods )---
+
+
+
+
+	public static final void testUtil (IData pipeline)
+        throws ServiceException
+	{
+		// --- <<IS-START(testUtil)>> ---
+		// @sigtype java 3.5
+		// --- <<IS-END>> ---
+
+                
+	}
+}
+
